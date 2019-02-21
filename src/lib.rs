73b5d667d206783f3cbe8bb4d1fc07785a8b63fd
Nikolay Kim <fafhrd91@gmail.com>
2019-02-20 21:33:02 -0800

    export ServiceRequest and ServiceResponse

--- src/lib.rs
+++ src/lib.rs
@@ -22,15 +22,18 @@ mod state;
 pub use actix_http::Response as HttpResponse;
 pub use actix_http::{http, Error, HttpMessage, ResponseError};
 
-pub use crate::app::{App, AppService};
+pub use crate::app::App;
 pub use crate::extractor::{Form, Json, Path, Query};
 pub use crate::handler::FromRequest;
 pub use crate::request::HttpRequest;
 pub use crate::resource::Resource;
 pub use crate::responder::{Either, Responder};
+pub use crate::service::{ServiceRequest, ServiceResponse};
 pub use crate::state::State;
 
 pub mod dev {
+    pub use crate::app::AppService;
     pub use crate::handler::{AsyncFactory, Extract, Factory, Handle};
+    pub use crate::route::{Route, RouteBuilder};
     // pub use crate::info::ConnectionInfo;
 }
